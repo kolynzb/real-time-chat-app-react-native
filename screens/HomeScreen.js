fb64aaa7719c22f3e38f7fae26ed7f519e6a885b
kolynzb <collinsbenda360@gmail.com>
2022-01-23 15:27:25 +0300

    add chat creen begin

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -4,10 +4,11 @@ import CustomListItem from "../components/CustomListItem";
 import { Avatar } from "react-native-elements";
 import { auth } from "../firebase";
 import { TouchableOpacity } from "react-native";
+import { AntDesign, SimpleLineIcons } from "@expo/vector-icons";
 import { signOut } from "firebase/auth";
 const HomeScreen = ({ navigation }) => {
   const signOutUser = () => {
-    signOut.then(() => {
+    signOut(auth).then(() => {
       navigation.replace("Login");
     });
   };
@@ -28,6 +29,26 @@ const HomeScreen = ({ navigation }) => {
                   "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6pcSbcsiQ-fF4z-gw_0kAIG0uhEKavZv7fKmMm8RH890YyvH3Z2g0T9QJwgEIG8qMT9Q&usqp=CAU",
               }}
             />
+          </TouchableOpacity>
+        </View>
+      ),
+      headerRight: () => (
+        <View
+          style={{
+            flexDirection: "row",
+            justifyContent: "space-between",
+            width: 80,
+            marginRight: 20,
+          }}
+        >
+          <TouchableOpacity activeOpacity={0.5}>
+            <AntDesign name="camerao" size={24} color="black" />
+          </TouchableOpacity>
+          <TouchableOpacity
+            activeOpacity={0.5}
+            onPress={() => navigation.navigate("AddChat")}
+          >
+            <SimpleLineIcons name="pencil" size={24} color="black" />
           </TouchableOpacity>
         </View>
       ),
